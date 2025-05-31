1038d48fe8ea48323f5a393a09bbd949f0472231
unknown <you@example.com>
2025-05-31 20:55:27 +0530

    footer and responsiveness changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 function Footer() {
   return (
-    <div className="footertext">
-      © {new Date().getFullYear()} XYZ SPACE. All rights reserved.
-    </div>
+   <footer className="footer">
+  <div className="footertext-left">
+    © {new Date().getFullYear()} XYZ SPACE
+  </div>
+  <div className="footertext-right" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+    Back to Top <span className="arrow-up">↑</span>
+  </div>
+</footer>
+
   );
 }
 
